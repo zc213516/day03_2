665e456c389e9f7982dda88dc41142ebee44464e
zc213516 <1830498798@qq.com>
2020-12-18 11:02:35 +0800

    我第四次提交了

--- app/src/main/java/com/example/day03_2/presenter/MainPresenter.java
+++ app/src/main/java/com/example/day03_2/presenter/MainPresenter.java
@@ -22,6 +22,7 @@ public class MainPresenter extends BasePresenter<MainContract.IMainView> impleme
 
 
     //你猜猜我改了什么
+    //是是是
     @Override
     public void login(String name, String password) {
        mainModel.getLoginData("", new ResultCallback<UserBean>() {
